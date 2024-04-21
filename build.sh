60990295fbd3e4f18fef6cf9d029273bf54d2664
xuruilong <2996613775@qq.com>
2024-04-21 21:00:31 +0800

    添加打包脚本

---
+++ build.sh
@@ -0,0 +1,1 @@
+pyinstaller --onefile --windowed --noconsole  -p "/Users/xuruilong/PycharmProjects/auto_save/.venv;" "/Users/xuruilong/PycharmProjects/auto_save/main.py"  -d all -F -n alexxu --clean
